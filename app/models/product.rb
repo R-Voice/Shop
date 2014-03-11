6e694e8f6c59f8116acdd7dfcaf97516ef945359
Shnip Den <asgardcafe@gmail.com>
2014-03-12 00:35:16 +0400

    start to register the order

---
+++ app/models/product.rb
@@ -0,0 +1,22 @@
+class Product < ActiveRecord::Base
+	validates :title, :discription, :image_url, :price, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates	:title, uniqueness:true
+	IMAGE_FORMAT = %r{\.(gif|jpg|png)\z}i
+	validates :image_url, allow_blank: true, format: {with: IMAGE_FORMAT}
+	 # message: "URL должен указывать на изображение формата GIF, JPG, PNG."}
+
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+	private
+
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, "существуют товарные позиции")
+			return false
+		end
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'b02574eb8ce0a961f143b5bae7bde770916fb045433f94af43293c3dc29c9877ba0229b79f91964853cfb676a7d9a74e2298aa98dd729bb96a1a72aa00423fbc'
